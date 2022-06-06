40521b2ef42ba497a523c4c6b5fcdf193fa52163
OgutuJoseph <onyango.joseph@yahoo.com>
2022-06-06 10:27:48 +0300

    Send Email (Not Working)

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,9 +1,51 @@
 'use strict'
 
+const nodemailer = require('nodemailer');
+const config = require('../helpers/config');
+
+const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: config.emailserver,
+        pass: config.emailpassword
+    }
+})
+
 const emailView = (req, res, next) => {
     res.render('email');
 }
 
+const sendEmail = (req, res, next) => {
+    try {
+        const { emailobject } = req.body;
+        const files = req.files;
+        if (emailobject) {
+            const email = JSON.parse(emailobject);
+            const mailoptions = {
+                from: config.emailserver,
+                to: email.emailto,
+                cc: email.emailcc,
+                bcc: email.emailbcc,
+                subject: email.subject,
+                html: email.body,
+                attachments: files
+
+            }
+            transporter.sendEmail(mailoptions, function(err, info){
+                if (error) {
+                    console.log('err: ', error)
+                }else {
+                    console.log('Email Sent' + info.response);
+                }
+            });
+            res.status(200).send('Email Sent Successfully.')
+        }
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+
 module.exports = {
-    emailView
+    emailView,
+    sendEmail,
 }
---
+++ helpers/filehelper.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
+    }
+})
+
+const filefilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' 
+    || file.mimetype === 'image/jpg' 
+    || file.mimetype === 'image/jpeg' 
+    || file.mimetype === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    || file.mimetype === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    || file.mimetype === 'application/pdf'
+    || file.mimetype === 'video/mp4'
+    || file.mimetype === 'audio/mp3') {
+        cb(null, true);
+    }else {
+        cb(null, false);
+    }
+}
+
+const upload = multer({ storage: storage, fileFilter: filefilter });
+
+module.exports = { upload };
